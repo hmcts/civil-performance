736720856726255af34b630a7c71df68263ba18c
amitsMOJ <amit.sinha1@hmcts.net>
2024-11-01 08:51:40 +0000

    2

--- src/gatling/simulations/uk/gov/hmcts/reform/civildamage/performance/simulations/scenarios/utils/spec_CL2_Headers.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/civildamage/performance/simulations/scenarios/utils/spec_CL2_Headers.scala
@@ -50,12 +50,19 @@ object spec_CL2_Headers{
     //
   )
 
-
   val headers_37 = Map(
 //    "content-type" -> "multipart/form-data; boundary=----WebKitFormBoundaryzGvBAZBXDDUaRq3D",
     "Sec-Fetch-Dest" -> "empty",
     "Sec-Fetch-Mode" -> "cors",
     "Sec-Fetch-Site" -> "same-origin"
   )
-
+  val headers_38 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.create-event.v2+json;charset=UTF-8",
+    "Content-Type" -> "application/json",
+    "Priority" -> "u=0",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "experimental" -> "true"
+  )
 }
