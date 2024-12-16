7f769ee57c9b1f0b99903e2971a3a8d98cb64079
jmcadam <mcadam88@gmail.com>
2024-12-16 17:07:04 +0000

    Update CUIR2Login.scala

--- src/gatling/simulations/scenarios/CUIR2Login.scala
+++ src/gatling/simulations/scenarios/CUIR2Login.scala
@@ -106,7 +106,8 @@ object CUIR2Login {
         .get(manageCaseURL + "/aggregated/caseworkers/:uid/jurisdictions?access=read")
         .headers(Headers.commonHeader)
         .header("accept", "application/json")
-        .check(substring("id")))
+        .check(substring("id"))
+        .check(status.in(200,204,302,304,401)))
 
       .exec(getCookieValue(CookieKey("XSRF-TOKEN").withDomain(manageCaseURL.replace("https://", "")).saveAs("XSRFToken")))
 
