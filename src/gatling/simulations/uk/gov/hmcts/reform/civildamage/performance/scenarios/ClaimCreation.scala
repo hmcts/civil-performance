d17608b6f6dbe617d0448f422f28bf100d233e74
yogeshruperai <yogesh.ruperai@solirius.com>
2025-07-31 10:38:58 +0100

    WIP - Adding dynatrace headers

--- src/gatling/simulations/uk/gov/hmcts/reform/civildamage/performance/scenarios/ClaimCreation.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/civildamage/performance/scenarios/ClaimCreation.scala
@@ -28,8 +28,7 @@ object ClaimCreation {
       exec(http("Civil_CreateClaim_030_CreateCase")
         .get("/aggregated/caseworkers/:uid/jurisdictions?access=create")
         .headers(CivilDamagesHeader.headers_104)
-        .header("x-dynatrace-test", "LTN=Pipeline;LSN=Civil;TSN=02_CreateCase")
-        .header("x-dynatrace-test", "TSN=02CreateCase")
+        .header("x-dynatrace-test", "TSN=03CreateCase;LSN=Civil;LTN=Pipeline")
         //.header("x-dynatrace-test", "FW4;TSN=CivilOnDynatrace;PSL=CitizenCreateClaimOnDynatrace")
         .check(status.in(200, 304))
       ).exitHereIfFailed
