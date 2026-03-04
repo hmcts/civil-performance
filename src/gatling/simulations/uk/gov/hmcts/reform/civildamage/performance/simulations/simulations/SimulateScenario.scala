a3efaacd23d89f1121bceaa2e9109dd7ca1ef1c7
pkryonas <paris.kryonas@solirius.com>
2026-03-04 12:27:28 +0000

    Modified scenarios for unspecified claim creation stress testing.

--- src/gatling/simulations/uk/gov/hmcts/reform/civildamage/performance/simulations/simulations/SimulateScenario.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/civildamage/performance/simulations/simulations/SimulateScenario.scala
@@ -35,61 +35,6 @@ class SimulateScenario extends Simulation {
     .exec(Logout.Signout)
     .pause(MaxThinkTime)
 
-//============02. Respond to Claimant==================
-    .exec(_.set("loginFlag", "defendant"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(unspec_DF1_resp.DF_Resp)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//===========03. Claimant Response to DF================
-    .exec(_.set("loginFlag", "claimant"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(unspec_CL1_Resp.RespToDF)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//============04. Judge SDO==============================
-    .exec(_.set("loginFlag", "judge"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(unspec_Jud_Hear.sdoJudge)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//============05. Hearing Admin===========================
-    .exec(_.set("loginFlag", "admin"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(unspec_Jud_Hear.HearingAdmin)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//=============06. Respondent Doc Upload===================
-    .exec(_.set("loginFlag", "defendant"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(unspec_DF2_CL2_FinalOrder_Not_In_Scope.DF_upload)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//==============07. Claimant Doc Upload====================
-    .exec(_.set("loginFlag", "claimant"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(unspec_DF2_CL2_FinalOrder_Not_In_Scope.CL_upload)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//===============08. Judge Final Order=====================
-    .exec(_.set("loginFlag", "judge"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(unspec_DF2_CL2_FinalOrder_Not_In_Scope.FinalOrder)
-    .exec(Logout.Signout)
-
 
 
   //==================Specified=============================
@@ -105,63 +50,7 @@ class SimulateScenario extends Simulation {
     .exec(Logout.Signout)
     .pause(MaxThinkTime)
 
-//==========02. Def Response==================
-    .exec(_.set("loginFlag", "defendant"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(spec_DF1_Resp.selectRespondToClaim)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//==========03. Claimant Response==================
-    .exec(_.set("loginFlag", "claimant"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(spec_CL1_Resp.RespToDef)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//==========04. Judge SDO==================
-    .exec(_.set("loginFlag", "judge"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(spec_SDO_Judge.sdoJudge)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//==========05.Hearing Schedule==================
-    .exec(_.set("loginFlag", "admin"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(spec_HearingAdmin.HearingAdmin)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//==========06. Respondent Doc Upload==================
-    .exec(_.set("loginFlag", "defendant"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(spec_DF2_CL2_FinalOrder_Not_In_Scope.DF_upload)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//==========07. Claimant Doc Upload==================
-    .exec(_.set("loginFlag", "claimant"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(spec_DF2_CL2_FinalOrder_Not_In_Scope.CL_upload)
-    .exec(Logout.Signout)
-    .pause(MaxThinkTime)
-
-//==========08. Judge Final Order=====================
-    .exec(_.set("loginFlag", "judge"))
-    .exec(Home.Homepage)
-    .exec(Login.Loginpage)
-    .exec(spec_DF2_CL2_FinalOrder_Not_In_Scope.FinalOrder)
-    .exec(Logout.Signout)
-
   setUp(
-    CreateUnSpecClaimSCN.inject(nothingFor(120),rampUsers(40).during(2500)),
-    CreateSpecClaimSCN.inject(rampUsers(40).during(2650))
+    CreateUnSpecClaimSCN.inject(rampUsers(300).during(300))
   ).protocols(httpProtocol)
 }
